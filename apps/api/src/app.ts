bab41fe1caaaf9182b33cc412a6c5abe8eab585e
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-06 02:12:29 -0300

    feat: update email format in client.http and add zod-validation-error dependency

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
+import { fromZodError } from 'zod-validation-error'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: fromZodError(error),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: here we should log the error to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
+})
