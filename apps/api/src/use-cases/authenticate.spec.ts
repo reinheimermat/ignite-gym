8b21c12334c932671a591fa2b2b9e365368a2510
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-08 23:59:07 -0300

    feat: implement authentication use case and add corresponding tests

---
+++ apps/api/src/use-cases/authenticate.spec.ts
@@ -0,0 +1,56 @@
+import { describe } from 'node:test'
+import { hash } from 'bcryptjs'
+import { expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hash('password123', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await expect(() =>
+      sut.execute({
+        email: 'wrong.email@example.com',
+        password: 'password123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hash('password123', 6),
+    })
+
+    await expect(() =>
+      sut.execute({
+        email: 'john.doe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
