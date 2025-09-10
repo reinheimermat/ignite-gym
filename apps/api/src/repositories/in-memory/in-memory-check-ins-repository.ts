8497336a5f7f3abc99f13d79cc8cddcbf8227076
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-09 23:59:49 -0300

    feat(api): Implements checkins history use case

--- apps/api/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ apps/api/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -40,4 +40,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
+  async findManyByUserId(userId: string, page: number): Promise<Checkin[]> {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 }
