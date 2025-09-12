22cefc5d4b98005b29b9c2f56eefd895e09fa896
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-12 19:50:34 -0300

    feat(tests): add end-to-end tests for user authentication flow

---
+++ apps/api/src/http/controllers/authenticate.e2e.spec.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
