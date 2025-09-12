21d2e0d86fa8af417742ca40f62f7744fcbcd4f7
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-12 00:16:14 -0300

    feat(api): add JWT verification middleware and protect profile route

--- apps/api/src/http/routes.ts
+++ apps/api/src/http/routes.ts
@@ -2,10 +2,17 @@ import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.post('/me', profile)
+  app.post(
+    '/me',
+    {
+      onRequest: [verifyJWT],
+    },
+    profile,
+  )
 }
