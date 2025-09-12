e60e96a844ffa97eb23d1ca67a3bed738a5f3ac3
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-11 21:20:41 -0300

    feat(api): implement PrismaGymsRepository with methods for finding, creating, and searching gyms

---
+++ apps/api/src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,52 @@
+import type { Gym, Prisma } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
+import type { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE (
+        6371 * acos(
+          cos(radians(${params.latitude})) * cos(radians(latitude)) *
+          cos(radians(longitude) - radians(${params.longitude})) +
+          sin(radians(${params.latitude})) * sin(radians(latitude))
+        )
+      ) <= 10
+    `
+
+    return gyms
+  }
+}
