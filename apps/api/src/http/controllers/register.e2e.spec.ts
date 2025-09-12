f1b5e246caba7a4623ef25af120b82f87c23208c
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-12 19:40:23 -0300

    feat(api): add supertest for end-to-end testing of user registration
    - Updated package.json to include supertest and its types.
    - Implemented end-to-end tests for user registration in register.e2e.spec.ts.
    - Added tests to verify successful registration and handling of duplicate email registrations.

--- apps/api/src/http/controllers/register.e2e.spec.ts
+++ apps/api/src/http/controllers/register.e2e.spec.ts
@@ -1,5 +1,38 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('testing', () => {
-  expect(true).toBe(true)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    })
+
+    expect(response.statusCode).toEqual(409)
+  })
 })
