9b425cd824de5ba390bdbe6fb42855fedc94f27f
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-14 00:52:27 -0300

    feat(api): add endpoint to fetch user check-in history with pagination

--- apps/api/src/http/controllers/check-ins/routes.ts
+++ apps/api/src/http/controllers/check-ins/routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
+import { history } from './history'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
+  app.get('/check-ins/history', history)
 }
