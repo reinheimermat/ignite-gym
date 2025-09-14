5a0d7cb5d6abaa8c74eff15d7f33051ab322b8dd
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-14 00:53:00 -0300

    feat(api): add validation endpoint for check-ins

--- apps/api/src/http/controllers/check-ins/routes.ts
+++ apps/api/src/http/controllers/check-ins/routes.ts
@@ -2,10 +2,12 @@ import type { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { history } from './history'
+import { validate } from './validate'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
   app.get('/check-ins/history', history)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
