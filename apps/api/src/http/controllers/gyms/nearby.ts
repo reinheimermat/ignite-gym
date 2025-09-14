6934e0cb7685d1327c335b969c47d20ad6a47ee8
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-14 00:41:08 -0300

    feat(api): add nearby and search gym endpoints with validation and use case integration

---
+++ apps/api/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((val) => {
+      return Math.abs(val) <= 90
+    }),
+    longitude: z.number().refine((val) => {
+      return Math.abs(val) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
